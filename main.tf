158a2889f72ed04115d62fa7e7f1331f6d4a343e
Aaron Mathis <aaron.mathis@gmail.com>
2024-08-05 22:48:18 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+  region     = "us-east-1"
+
+
+}
+
+resource "aws_instance" "Webserver" {
+  ami               = "ami-04a81a99f5ec58529"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+
+}
