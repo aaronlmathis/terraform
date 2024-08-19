01e7937871623a826ae11027ee8f0da04ce404dd
Aaron Mathis <aaron.mathis@gmail.com>
2024-08-19 11:02:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,53 +8,56 @@ resource "aws_instance" "Webserver" {
   availability_zone = "us-east-1a"
   key_name          = "Terraform"  # Remove this line if SSH access is not needed
   vpc_security_group_ids = ["sg-00210c164f22a8dc2"]
-
+  
   user_data = <<-EOF
               #!/bin/bash
               # Update the system
               sudo apt-get update -y
 
               # Install required packages
-              #sudo apt-get install -y git python3 python3-pip python3-venv nginx
-
-              # Create a virtual environment
-              #python3 -m venv /home/debian/flaskapp/venv
-
-              # Activate the virtual environment
-              #source /home/debian/flaskapp/venv/bin/activate
-
-              # Install Gunicorn in the virtual environment
-              #pip install gunicorn
+              sudo apt-get install -y git python3 python3-pip python3-venv nginx
 
               # Clone the GitHub repository
-              #git clone https://github.com/aaronlmathis/flaskapp.git /home/debian/flaskapp
+              git clone https://github.com/aaronlmathis/flaskapp.git /home/admin/flaskapp
+
+              # Create a virtual environment
+              python3 -m venv /home/admin/flaskapp/.venv
+
+              # Activate the virtual environment
+              source /home/admin/flaskapp/.venv/bin/activate
+
+              # Install Gunicorn in the virtual environment
+              pip install gunicorn
 
               # Change directory to the app directory
-              #cd /home/debian/flaskapp
+              cd /home/admin/flaskapp
 
               # Install Python dependencies inside the virtual environment
-              #pip install -r requirements.txt
+              pip install -r requirements.txt
 
               # Set up Gunicorn to serve the Flask app using the virtual environment
-              #/home/debian/flaskapp/venv/bin/gunicorn --workers 3 --bind unix:/home/debian/flaskapp/flaskapp.sock -m 007 app:app --daemon
+              /home/admin/flaskapp/.venv/bin/gunicorn --workers 3 --bind unix:/home/admin/flaskapp/flaskapp.sock -m 007 "website:create_app()" --daemon
+
+              # Set the correct permissions on the Unix socket
+              sudo chmod 666 /home/admin/flaskapp/flaskapp.sock
 
               # Configure Nginx to reverse proxy to Gunicorn
-              #sudo bash -c 'cat > /etc/nginx/sites-available/flaskapp << EOF
-              #server {
-              #   listen 80;
-              #    server_name _;
+              sudo tee /etc/nginx/sites-available/flaskapp > /dev/null <<EOF
+              server {
+                  listen 80;
+                  server_name _;
 
-              #    location / {
-              #        include proxy_params;
-              #        proxy_pass http://unix:/home/debian/flaskapp/flaskapp.sock;
-              #    }
-              #}
-              #EOF'
+                  location / {
+                      include proxy_params;
+                      proxy_pass http://unix:/home/admin/flaskapp/flaskapp.sock;
+                  }
+              }
+              EOF
 
               # Enable the Nginx configuration and restart the service
-              #sudo ln -s /etc/nginx/sites-available/flaskapp /etc/nginx/sites-enabled
-              #sudo nginx -t
-              #sudo systemctl restart nginx
+              sudo ln -s /etc/nginx/sites-available/flaskapp /etc/nginx/sites-enabled
+              sudo nginx -t
+              sudo systemctl restart nginx
 
               EOF
 
